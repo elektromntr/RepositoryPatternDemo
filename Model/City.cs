1b436fe0d17c538c686c6e271f1f52ad3950ae77
elektromntr <bartosztadeuszwojcik@gmail.com>
2018-08-04 15:56:34 +0200

    dodane klasy modeli

---
+++ Model/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class City
+    {
+        public int CityID { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Hero> Heroes { get; set; }
+    }
+}
---
+++ Model/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Hero
+    {
+        public int HeroID { get; set; }
+        public string Name { get; set; }
+        public decimal Age { get; set; }
+        
+        public int Fitness { get; set; }
+        public int Mind { get; set; }
+        public int Emphaty { get; set; }
+
+        public int CityID { get; set; }
+        public City City { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateCreated()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
