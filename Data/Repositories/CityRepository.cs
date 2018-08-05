f7f7c4989b027310198f5c64f0a1b979d5302601
elektromntr <bartosztadeuszwojcik@gmail.com>
2018-08-05 12:55:22 +0200

    Dodane Reositories: Hero, City

---
+++ Data/Repositories/CityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class CityRepository : RepositoryBase<City>, ICityRepository
+    {
+        public CityRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+        public City GetCityByName(string cityName)
+        {
+            var city = this.DbContext.City.Where(c => c.Name == cityName).FirstOrDefault();
+
+            return city;
+        }
+
+        public interface ICityRepository : IRepository<City>
+        {
+            City GetCityByName(string cityName);
+        }
+    }
+}
---
+++ Data/Repositories/HeroRepository.cs
@@ -0,0 +1,34 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class HeroRepository : RepositoryBase<Hero>, IHeroRepository
+    {
+        public HeroRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+
+    public Hero GetHeroByName(string heroName)
+    {
+        var hero = this.DbContext.Heroes.Where(h => h.Name == heroName).FirstOrDefault();
+
+        return hero;
+    }
+
+    public Hero GetHeroesByCity(string cityId)
+    {
+        var heroes = this.DbContext.Heroes.Where(h => h.CityID == cityId);
+
+        return heroes.ToList();
+    }
+
+    public interface IHeroRepository : IRepository<Hero>
+    {
+
+    }
+}
