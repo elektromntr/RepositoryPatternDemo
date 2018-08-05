eef906f6fffc4fa0c612a932d033e025ccf734a2
elektromntr <bartosztadeuszwojcik@gmail.com>
2018-08-05 12:01:19 +0200

    InfrastructurDalsza rozbudowa Infrastructure, IRepository, IUnitOfWork, RepositoryBase, UnitOfWork

---
+++ Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private RepoPatternDemoEntities dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public RepoPatternDemoEntities DbContext
+        {
+            get { return dbContext ?? (dbContext = dbContext.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.Commit();
+        }
+    }
+}
